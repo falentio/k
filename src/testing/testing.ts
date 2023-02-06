5f782ea7f615904c4149c5abb488bb1f2e57f154
falentio <riv.kevinfalent.io@gmail.com>
2023-02-06 22:18:06 +0700

    init

--- src/testing/testing.ts
+++ src/testing/testing.ts
@@ -1,7 +1,7 @@
 import postgres from "postgresjs";
 
 const sql = postgres(
-  "postgresql://Falentio:v2_3ygut_8GDn3zLsjw3rfUp9bvub7dH@db.bit.io:5432/Falentio/k",
+  Deno.env.get("DATABASE_DSN"),
   {
     ssl: { rejectUnauthorized: false },
   },
